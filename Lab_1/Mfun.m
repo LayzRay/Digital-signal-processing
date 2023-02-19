68e3710c2b4de5cf52fc05be9a87473649f89556
LayzRay <an.goleff@yandex.ru>
2023-02-19 11:48:39 +0300

    Выполнил две задачи

--- Lab_1/Mfun.m
+++ Lab_1/Mfun.m
@@ -1,3 +1,24 @@
-function [ num ] = Mfun ( A, C )
+function [ M ] = Mfun ( A, C )
 
-num = A + C;
+    m = length( C );
+    
+    M = zeros(1,m);
+    
+    for n = 1 : 2 ^ ( m ) - 1
+    
+        t = A & C;
+    
+        s = 0;
+    
+        for i = 1:length( t )
+            s = xor(s, t( i ) );
+        end
+    
+        M(n) = s;
+    
+        A = circshift(A, 1);
+        A(1) = s;
+    
+    end
+
+end
---
+++ Lab_1/SubM.m
@@ -0,0 +1,20 @@
+function [ M ] = SubM( M )
+% Заменяет в M-последовательности 0 на 1, 1 на -1.
+
+
+    for i = 1:length( M )
+    
+        if ( M( i ) )
+    
+            M( i ) = -1;
+    
+        else
+    
+            M( i ) = 1;
+    
+        end
+    
+    
+    end
+
+end
