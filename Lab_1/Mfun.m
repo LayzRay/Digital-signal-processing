b56bb0f0e1b35dd73eca7725a57a04014bcc9521
LayzRay <an.goleff@yandex.ru>
2023-02-20 11:45:01 +0300

    Первая часть лабораторной готова и сдана

--- Lab_1/Mfun.m
+++ Lab_1/Mfun.m
@@ -5,13 +5,13 @@ function [ M ] = Mfun ( A, C )
 
     m = length( C );
     
-    M = zeros( 1, m );
+    M = zeros( 1, 2 ^ ( m ) - 1 );
     
     for n = 1 : 2 ^ ( m ) - 1
 
         M( n ) = A( length( A ) );
 
-        temp = A & C( 2 : length( C ) ); 
+        temp = A & C; 
     
         A = circshift( A, 1 );
         A( 1 ) = mod( sum( temp ), 2 ); 
--- Lab_1/my_sf.m
+++ Lab_1/my_sf.m
@@ -4,13 +4,13 @@ function[ Filtered_signal ] = my_sf( A, B )
     t = zeros( 1, m );
     Filtered_signal = zeros( 1, 2 *m );
 
-    A = [ A, zeros(1,m) ];
+    A = [ zeros( 1, m ), A ];
     
     for n = 1 : 2 * m
 
         Filtered_signal( n ) = sum ( t .* B );
         t = circshift( t, 1 );
-        t( 1 ) = A( n );
+        t( 1 ) = A( 2 * m - n + 1 );
         
     end
 
